fb97fe0fb1ce8522b938ee776835392a3919eac4
John Moran <johnnymoran44@hotmail.com>
2019-05-20 16:11:50 +0200

    moved the map data to app.js, working

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.scss";
 import MapContainer from "./components/map";
 import Car2GoMain from "./components/Car2GoMain";
@@ -26,14 +26,37 @@ library.add(
   faTimes
 );
 
-function App() {
-  return (
-    <div className="App">
-      <Car2GoMain />
-      <MyTaxiMain />
-      <MapContainer />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.onMarkerClick = this.onMarkerClick.bind(this);
+    this.state = {
+      showingInfoWindow: false,
+      activeMarker: {},
+      selectedPlace: {}
+    };
+  }
+  onMarkerClick(props, marker, e) {
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true
+    });
+  }
+  render() {
+    return (
+      <div className="App">
+        <Car2GoMain />
+        <MyTaxiMain />
+        <MapContainer
+          selectedPlace={this.state.selectedPlace}
+          activeMarker={this.state.activeMarker}
+          showingInfoWindow={this.state.showingInfoWindow}
+          handleClick={this.onMarkerClick}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/map.js
+++ src/components/map.js
@@ -1,12 +1,12 @@
-import React, { Component } from "react";
+import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Car2go from "../data/car2go/vehicles.json";
 import MyTaxiData from "../data/mytaxi/vehicles.json";
 import { Map, InfoWindow, Marker, GoogleApiWrapper } from "google-maps-react";
 import "./stylesheets/map.scss";
 
-export class MapContainer extends Component {
-  constructor(props) {
+const MapContainer = props => {
+  /* constructor(props) {
     super(props);
     this.onMarkerClick = this.onMarkerClick.bind(this);
     this.state = {
@@ -21,140 +21,137 @@ export class MapContainer extends Component {
       activeMarker: marker,
       showingInfoWindow: true
     });
-  }
-  render() {
-    const google = window.google;
-    const style = {
-      margin: "0 auto"
-    };
-    //const google = window.google;
-    return (
-      <div className="map-class">
-        <Map
-          google={this.props.google}
-          style={style}
-          initialCenter={{
-            lat: 53.5511,
-            lng: 9.9937
-          }}
-          zoom={13}
+  } */
+
+  const google = window.google;
+  const style = {
+    margin: "0 auto"
+  };
+  //const google = window.google;
+  return (
+    <div className="map-class">
+      <Map
+        google={props.google}
+        style={style}
+        initialCenter={{
+          lat: 53.5511,
+          lng: 9.9937
+        }}
+        zoom={13}
+      >
+        {Car2go.placemarks.map((content, index) => {
+          return (
+            <Marker
+              title={(index + 1).toString()}
+              name={content.name}
+              interior={content.interior}
+              exterior={content.exterior}
+              type="car2go"
+              position={{
+                lat: content.coordinates[1],
+                lng: content.coordinates[0]
+              }}
+              onClick={props.handleClick}
+              icon={{
+                url: require("../images/car2go-pin.svg"),
+                anchor: new google.maps.Point(5, 58)
+              }}
+              key={index}
+            />
+          );
+        })}
+        {MyTaxiData.poiList.map((TaxiData, index) => {
+          return (
+            <Marker
+              title={(index + 1).toString()}
+              name={TaxiData.id}
+              type="my-taxi"
+              position={{
+                lat: TaxiData.coordinate.latitude,
+                lng: TaxiData.coordinate.longitude
+              }}
+              onClick={props.handleClick}
+              icon={{
+                url: require("../images/my-taxi-pin.svg"),
+                anchor: new google.maps.Point(5, 58)
+              }}
+              key={index}
+              activetaxi={TaxiData.state}
+            />
+          );
+        })}
+
+        <InfoWindow
+          marker={props.activeMarker}
+          visible={props.showingInfoWindow}
         >
-          {Car2go.placemarks.map((content, index) => {
-            return (
-              <Marker
-                title={(index + 1).toString()}
-                name={content.name}
-                interior={content.interior}
-                exterior={content.exterior}
-                type="car2go"
-                position={{
-                  lat: content.coordinates[1],
-                  lng: content.coordinates[0]
-                }}
-                onClick={this.onMarkerClick}
-                icon={{
-                  url: require("../images/car2go-pin.svg"),
-                  anchor: new google.maps.Point(5, 58)
-                }}
-                key={index}
-              />
-            );
-          })}
-          {MyTaxiData.poiList.map((TaxiData, index) => {
-            return (
-              <Marker
-                title={(index + 1).toString()}
-                name={TaxiData.id}
-                type="my-taxi"
-                position={{
-                  lat: TaxiData.coordinate.latitude,
-                  lng: TaxiData.coordinate.longitude
-                }}
-                onClick={this.onMarkerClick}
-                icon={{
-                  url: require("../images/my-taxi-pin.svg"),
-                  anchor: new google.maps.Point(5, 58)
-                }}
-                key={index}
-                activetaxi={TaxiData.state}
-              />
-            );
-          })}
-
-          <InfoWindow
-            marker={this.state.activeMarker}
-            visible={this.state.showingInfoWindow}
-          >
-            <div>
-              <p>
-                {this.state.selectedPlace.interior === "GOOD" ? (
-                  <span className="pattern green">
-                    <FontAwesomeIcon icon="thumbs-up" />
-                  </span>
-                ) : (
-                  ""
-                )}
-
-                {this.state.selectedPlace.interior === "UNACCEPTABLE" ? (
-                  <span className="pattern red">
-                    <FontAwesomeIcon icon="thumbs-down" />
-                  </span>
-                ) : (
-                  ""
-                )}
-
-                {this.state.selectedPlace.activetaxi === "ACTIVE" ? (
-                  <span className="pattern green">
-                    <FontAwesomeIcon icon="check" />
-                  </span>
-                ) : (
-                  ""
-                )}
-                {this.state.selectedPlace.activetaxi === "INACTIVE" ? (
-                  <span className="pattern red">
-                    <FontAwesomeIcon icon="times" />
-                  </span>
-                ) : (
-                  ""
-                )}
-
-                {this.state.selectedPlace.type === "my-taxi" ? (
-                  <span className="pattern black-bg">
-                    {this.state.selectedPlace.title}{" "}
-                  </span>
-                ) : (
-                  <span className="pattern">
-                    {this.state.selectedPlace.title}
-                  </span>
-                )}
-              </p>
-              <p>
-                {this.state.selectedPlace.exterior === "GOOD" ? (
-                  <span className="pattern green">
-                    <FontAwesomeIcon icon="thumbs-up" />
-                  </span>
-                ) : (
-                  ""
-                )}
-
-                {this.state.selectedPlace.exterior === "UNACCEPTABLE" ? (
-                  <span className="pattern red">
-                    <FontAwesomeIcon icon="thumbs-down" />
-                  </span>
-                ) : (
-                  ""
-                )}
-                <span>
-                  <b>{this.state.selectedPlace.name}</b>
+          <div>
+            <p>
+              {props.selectedPlace.interior === "GOOD" ? (
+                <span className="pattern green">
+                  <FontAwesomeIcon icon="thumbs-up" />
                 </span>
-              </p>
-            </div>
-          </InfoWindow>
-        </Map>
-      </div>
-    );
-  }
-}
+              ) : (
+                ""
+              )}
+
+              {props.selectedPlace.interior === "UNACCEPTABLE" ? (
+                <span className="pattern red">
+                  <FontAwesomeIcon icon="thumbs-down" />
+                </span>
+              ) : (
+                ""
+              )}
+
+              {props.selectedPlace.activetaxi === "ACTIVE" ? (
+                <span className="pattern green">
+                  <FontAwesomeIcon icon="check" />
+                </span>
+              ) : (
+                ""
+              )}
+              {props.selectedPlace.activetaxi === "INACTIVE" ? (
+                <span className="pattern red">
+                  <FontAwesomeIcon icon="times" />
+                </span>
+              ) : (
+                ""
+              )}
+
+              {props.selectedPlace.type === "my-taxi" ? (
+                <span className="pattern black-bg">
+                  {props.selectedPlace.title}{" "}
+                </span>
+              ) : (
+                <span className="pattern">{props.selectedPlace.title}</span>
+              )}
+            </p>
+            <p>
+              {props.selectedPlace.exterior === "GOOD" ? (
+                <span className="pattern green">
+                  <FontAwesomeIcon icon="thumbs-up" />
+                </span>
+              ) : (
+                ""
+              )}
+
+              {props.selectedPlace.exterior === "UNACCEPTABLE" ? (
+                <span className="pattern red">
+                  <FontAwesomeIcon icon="thumbs-down" />
+                </span>
+              ) : (
+                ""
+              )}
+              <span>
+                <b>{props.selectedPlace.name}</b>
+              </span>
+            </p>
+          </div>
+        </InfoWindow>
+      </Map>
+    </div>
+  );
+};
 
 export default GoogleApiWrapper({
   apiKey: "AIzaSyBLKOadUkRR6mX4J28tju1U8XB9ohNZA94"
