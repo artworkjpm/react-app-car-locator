b1628930924da605b85a064d16d4e5c95d6c46d8
John Moran <johnnymoran44@hotmail.com>
2019-05-21 10:51:59 +0200

    ok from here MAKE THE CLICK

--- src/components/Car2GoMain.js
+++ src/components/Car2GoMain.js
@@ -1,67 +1,65 @@
-import React, { Component } from "react";
+import React from "react";
 import Car2go from "../data/car2go/vehicles.json";
 import RenderTable from "./renderTable.js";
 import "./stylesheets/Main.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Carlogo from "../images/car-logo.svg";
 
-export class Car2GoMain extends Component {
-  render() {
-    return (
-      <div className="component-wrapper">
-        <div className="wrapper-main">
-          <div className="header-main">
-            <div className="header">
-              <div className="row">
-                <div className="column-left">
-                  <img src={Carlogo} width="80" alt="car2go logo" />
-                </div>
-                <div className="column-right">
-                  <span className="pattern">{Car2go.placemarks.length}</span>
-                  <b>Cars available</b>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="car2go wrapper">
-            {Car2go.placemarks.map((content, index) => {
-              return <RenderTable content={content} ind={index} key={index} />;
-            })}
-          </div>
-          <div className="explain">
+const Car2GoMain = props => {
+  return (
+    <div className="component-wrapper">
+      <div className="wrapper-main">
+        <div className="header-main">
+          <div className="header">
             <div className="row">
               <div className="column-left">
-                <p>
-                  <span className="pattern">1</span>
-                </p>
-
-                <p>
-                  <span className="pattern green">
-                    <FontAwesomeIcon icon="thumbs-up" />
-                  </span>
-                </p>
-
-                <p>
-                  <span className="pattern red">
-                    <FontAwesomeIcon icon="thumbs-down" />
-                  </span>
-                </p>
+                <img src={Carlogo} width="80" alt="car2go logo" />
               </div>
               <div className="column-right">
-                <p>Car number</p>
-                <p>
-                  <b>Interior</b> of car (rated good)
-                </p>
-                <p>
-                  <b>Exterior</b> of car (rated bad)
-                </p>
+                <span className="pattern">{Car2go.placemarks.length}</span>
+                <b>Cars available</b>
               </div>
             </div>
           </div>
         </div>
+        <div className="car2go wrapper">
+          {Car2go.placemarks.map((content, index) => {
+            return <RenderTable content={content} ind={index} key={index} />;
+          })}
+        </div>
+        <div className="explain">
+          <div className="row">
+            <div className="column-left">
+              <p>
+                <span className="pattern">1</span>
+              </p>
+
+              <p>
+                <span className="pattern green">
+                  <FontAwesomeIcon icon="thumbs-up" />
+                </span>
+              </p>
+
+              <p>
+                <span className="pattern red">
+                  <FontAwesomeIcon icon="thumbs-down" />
+                </span>
+              </p>
+            </div>
+            <div className="column-right">
+              <p>Car number</p>
+              <p>
+                <b>Interior</b> of car (rated good)
+              </p>
+              <p>
+                <b>Exterior</b> of car (rated bad)
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Car2GoMain;
